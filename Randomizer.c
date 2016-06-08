e9cc153937927a82506ab319e8993fc0aac8afa6
eminems95 <mrcn.mucha@gmail.com>
2016-06-09 01:47:43 +0200

    Program randomize one ASCII character from 65 to 126.

--- Randomizer.c
+++ Randomizer.c
@@ -1,16 +1,49 @@
-#include<stdio.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHMSIZE 20
+
+
+char RandomizeCharacter(char randomCharacter){
+	randomCharacter = (rand()%61)+65;
+	return randomCharacter;
+}
 
 
 int main(){
-srand(time(NULL));
-key_t key = 1234;
-int shmid;
-int i;
+	srand(time(NULL));
+	key_t key = 2137;
+	int sharedMemoryID;
+	char *sharedMemory;
+	char randomCharacter;
+	int sameCharacter=0;
+	int i;
 
+	sharedMemoryID = shmget(key, SHMSIZE, IPC_CREAT | 0666);
+	
+	if(sharedMemoryID < 0)
+	{
+		perror("Sharing memory error");
+		exit(1);
+	}
 
+	sharedMemory = shmat(sharedMemoryID, NULL, 0);
+	
+	if(sharedMemory == (char *) -1)
+	{
+		perror("Sharing memory segment error");
+		exit(1);
+	}
 
-
+	for(i=0;;i++){
+		sharedMemory[i%SHMSIZE]=RandomizeCharacter(randomCharacter);
+	}
+	
 
 return 0;
 }
--- Selector.c
+++ Selector.c
@@ -1,9 +1,51 @@
-#include<stdio.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHMSIZE 20
 
 int main(){
+srand(time(NULL));
+	key_t key = 2137;
+	int shmid;
+	char *sharedMemory;
+	char *text;
+	int sameCharacter=0;
+	int i;
 
+	shmid = shmget(key, SHMSIZE, IPC_CREAT | 0666);
+	
+	if(shmid < 0)
+	{
+		perror("Sharing memory error");
+		exit(1);
+	}
 
+	sharedMemory = shmat(shmid, NULL, 0);
+	
+	if(sharedMemory == (char *) -1)
+	{
+		perror("Sharing memory segment error");
+		exit(1);
+	}
+
+	for(i=0;;i++){
+		printf("%c",sharedMemory[i%SHMSIZE]);
+		
+		if(sharedMemory[i%SHMSIZE] == sharedMemory[i%SHMSIZE-1]){
+			sameCharacter++;
+		}else{
+			sameCharacter=0;
+		}
+		
+		if(sameCharacter==5){
+				
+		}
+	}
 
 return 0;
 }
